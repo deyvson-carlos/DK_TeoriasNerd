81c6c5773a031ad259f0b75d33aa9d34c703ea34
Deyvson Tenorio <deyvson.tenorio@nagem.com.br>
2022-10-13 14:22:50 -0300

    alteracção

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function mostrarSenha(){
+    var senha = document.getElementById("senha");
+    if (senha.type == "password"){
+         senha.type = "text";
+    } else {
+     senha.type = "password";
+    }
+ } 
+
+ function logar(){
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+
+    if(email == "teoriasnerd@dk.com" && senha == "123"){
+        alert('Usuário logado com Sucesso!');
+        location.href = "logado.html";
+    }
+    else{
+        alert('Usuário ou senha incorretos!');
+    }
+ }
